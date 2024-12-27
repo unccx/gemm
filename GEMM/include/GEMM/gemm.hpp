d3b1aac827f281c6b039ce2c0f139cbe3f002708
unccx <2336429253@qq.com>
2024-12-27 16:12:33 +0800

    🦄 refactor(gemm): 从显式实例化模型改为包含模型组织模板代码

--- GEMM/include/GEMM/gemm.hpp
+++ GEMM/include/GEMM/gemm.hpp
@@ -1,85 +1,90 @@
 #pragma once
+#include <GEMM/matrix_view.hpp>
 #include <GEMM/micro_kernel.hpp>
+#include <algorithm>
 #include <cassert>
 #include <cstddef>
+#include <cstdlib>
 
 namespace GEMM {
-enum class StorageLayout {
-  RowMajor = 0,   // 行优先
-  ColumnMajor = 1 // 列优先
-};
-
-template <typename T, StorageLayout layout = StorageLayout::ColumnMajor>
-class MatrixView {
-public:
-  size_t start_row, start_col;
-  size_t rows, cols;
-  size_t lda;
-  T *data;
-
-  /**
-   * @brief Construct a new Matrix View object
-   *
-   * @param rows 行数
-   * @param cols 列数
-   * @param data 指向数据的指针，
-   *             需要调用者保证 data 指针所持有的内存size >= rows*cols
-   * @param lda  leading dimension, 默认为 0
-   *             若为 0 则根据 RowMajor 或 ColumnMajor 选择 cols 或 rows
-   * @param start_row submatrix窗口的左上角坐标行
-   * @param start_col submatrix窗口的左上角坐标列
-   */
-  MatrixView(size_t rows, size_t cols, T *data, size_t lda = 0,
-             size_t start_row = 0, size_t start_col = 0)
-      : rows(rows), cols(cols), data(data), start_row(start_row),
-        start_col(start_col) {
-    if constexpr (layout == StorageLayout::RowMajor) {
-      this->lda = lda > 0 ? lda : cols;
-    } else {
-      this->lda = lda > 0 ? lda : rows;
+/**
+ * @brief 把矩阵按照行主序或列主序 pack into 到 packed_panel
+ *
+ * @tparam T
+ * @tparam layout
+ * @tparam stride
+ * @param sub_mat
+ * @param packed_panel 保存 packed data 的指针
+ *                     需要调用者保证指针所持有的内存size>=stride*k_c
+ */
+template <typename T, StorageLayout mat_layout, size_t stride,
+          StorageLayout panel_layout>
+MatrixView<T, panel_layout> pack_panel(const MatrixView<T, mat_layout> sub_mat,
+                                       T *packed_panel) {
+  auto pack_row_major = [&]() {
+    MatrixView<T, panel_layout> ret{sub_mat.rows, stride, packed_panel};
+    for (size_t i = 0; i < sub_mat.rows; i++) {
+      for (size_t j = 0; j < sub_mat.cols; j++) {
+        *(packed_panel++) = sub_mat.get(i, j);
+      }
+      for (size_t j = sub_mat.cols; j < stride; j++) {
+        *(packed_panel++) = sub_mat.get(i, 0);
+      }
     }
-  }
+    return ret;
+  };
 
-  T get(size_t i, size_t j) const { return data[calculateIndex(i, j)]; }
-
-  void set(size_t i, size_t j, T value) { data[calculateIndex(i, j)] = value; }
-
-  T *getAddress(size_t i, size_t j) const {
-    return &data[calculateIndex(i, j)];
-  }
-
-  /**
-   * @brief 生成子矩阵窗口
-   *
-   * @param i 窗口左上角坐标 (i, j)
-   * @param j 窗口左上角坐标 (i, j)
-   * @param rows 窗口行数
-   * @param cols 窗口列数
-   * @return MatrixView
-   */
-  MatrixView submat(size_t i, size_t j, size_t rows, size_t cols) {
-    assert((i + rows) * (j + cols) <= (this->rows) * (this->cols));
-    return MatrixView<T, layout>{rows, cols, data, lda, i, j};
-  }
-
-private:
-  /**
-   * @brief 计算第i行第j列的元素在一维内存中的索引，兼容RowMajor和ColumnMajor
-   *
-   * @param i 第i行
-   * @param j 第j列
-   * @return size_t
-   */
-  size_t calculateIndex(size_t i, size_t j) const {
-    assert(0 <= i && i < rows);
-    assert(0 <= j && j < cols);
-    if constexpr (layout == StorageLayout::RowMajor) {
-      return (start_row + i) * lda + (start_col + j);
-    } else {
-      return (start_col + j) * lda + (start_row + i);
+  auto pack_col_major = [&]() {
+    MatrixView<T, panel_layout> ret{stride, sub_mat.cols, packed_panel};
+    for (size_t j = 0; j < sub_mat.cols; j++) {
+      for (size_t i = 0; i < sub_mat.rows; i++) {
+        *(packed_panel++) = sub_mat.get(i, j);
+      }
+      for (size_t i = sub_mat.rows; i < stride; i++) {
+        *(packed_panel++) = sub_mat.get(0, j);
+      }
     }
+    return ret;
+  };
+
+  if constexpr (panel_layout == StorageLayout::RowMajor) {
+    return pack_row_major();
+  } else {
+    return pack_col_major();
   }
-};
+}
+
+/**
+ * @brief
+ *
+ * @tparam T
+ * @tparam GEMM_MR
+ * @tparam GEMM_NR
+ * @tparam MICRO_KERNEL
+ * @param m
+ * @param k
+ * @param n
+ * @param packA
+ * @param packB
+ * @param C
+ */
+template <typename T, StorageLayout layout, size_t GEMM_MR, size_t GEMM_NR,
+          typename MICRO_KERNEL>
+void macro_kernel(const size_t m, const size_t k, const size_t n, T *packA,
+                  T *packB, MatrixView<T, layout> C) {
+  // 2-th loop around micro-kernel
+  for (size_t j = 0; j < n; j += GEMM_NR) { // j 是 B_panel 中的列索引
+    // 1-th loop around micro-kernel
+    for (size_t i = 0; i < m; i += GEMM_MR) { // i 是 A_panel 中的行索引
+      MatrixView<T, StorageLayout::ColumnMajor> A_micro_panel{GEMM_MR, k,
+                                                              &packA[i * k]};
+      MatrixView<T, StorageLayout::RowMajor> B_micro_panel{k, GEMM_NR,
+                                                           &packB[j * k]};
+      MICRO_KERNEL::block_update(A_micro_panel, B_micro_panel,
+                                 C.submat(i, j, GEMM_MR, GEMM_NR));
+    } // 1-th loop around micro-kernel
+  } // 2-th loop around micro-kernel
+}
 
 /**
  * @brief 通用矩阵-矩阵乘法 A{m,k} x B{k,n} = C{m,n} ,矩阵元素为 T
@@ -96,9 +101,66 @@ private:
  * @param B 输入矩阵
  * @return MatrixView<T> 输出矩阵
  */
-template <typename T, size_t GEMM_NC = 4080, size_t GEMM_KC = 256,
-          size_t GEMM_MC = 72, size_t GEMM_MR = 8, size_t GEMM_NR = 4,
-          typename MICRO_KERNEL = MicroKernel<T, GEMM_MR, GEMM_NR>,
+template <typename T, StorageLayout layout = StorageLayout::ColumnMajor,
+          size_t GEMM_NC = 4080, size_t GEMM_KC = 256, size_t GEMM_MC = 72,
+          size_t GEMM_MR = 8, size_t GEMM_NR = 4,
+          typename MICRO_KERNEL = MicroKernel<T, layout, GEMM_MR, GEMM_NR>,
           size_t GEMM_SIMD_ALIGN_SIZE = 32>
-MatrixView<T> gemm(MatrixView<T> A, MatrixView<T> B);
+MatrixView<T, layout> gemm(const MatrixView<T, layout> A,
+                           const MatrixView<T, layout> B) {
+  // check shape
+  assert(A.cols == B.rows);
+  const auto &m = A.rows;
+  const auto &k = A.cols; // B.rows
+  const auto &n = B.cols;
+  if (m == 0 || k == 0 || n == 0) {
+    return {0, 0, nullptr};
+  }
+
+  // Allocate C matrix buffer
+  T *C_data = (T *)std::aligned_alloc(GEMM_SIMD_ALIGN_SIZE, m * n * sizeof(T));
+  MatrixView<T, StorageLayout::ColumnMajor> C{m, n, C_data};
+
+  // Allocate packing buffers
+  T *packA = (T *)std::aligned_alloc(GEMM_SIMD_ALIGN_SIZE,
+                                     (GEMM_MC + GEMM_MR) * GEMM_KC * sizeof(T));
+  T *packB = (T *)std::aligned_alloc(GEMM_SIMD_ALIGN_SIZE,
+                                     GEMM_KC * (GEMM_NC + GEMM_NR) * sizeof(T));
+
+  // 5-th loop around micro-kernel
+  for (size_t j_c = 0; j_c < n; j_c += GEMM_NC) {
+    size_t n_c = std::min(n - j_c, GEMM_NC);
+
+    // 4-th loop around micro-kernel
+    for (size_t p_c = 0; p_c < k; p_c += GEMM_KC) {
+      size_t k_c = std::min(k - p_c, GEMM_KC);
+      // pack into B_panel
+      for (size_t j = 0; j < n_c; j += GEMM_NR) { // j 是 B_panel 中的列索引
+        size_t n_r = std::min(n_c - j, GEMM_NR);
+        pack_panel<T, layout, GEMM_NR, StorageLayout::RowMajor>(
+            B.submat(p_c, j_c + j, k_c, n_r), &packB[j * k_c]);
+      }
+
+      // 3-rd loop around micro-kernel
+      for (size_t i_c = 0; i_c < m; i_c += GEMM_MC) {
+        size_t m_c = std::min(m - i_c, GEMM_MC);
+        // pack into A_panel
+        for (size_t i = 0; i < m_c; i += GEMM_MR) { // i 是 A_panel 中的行索引
+          size_t m_r = std::min(m_c - i, GEMM_MR);
+          pack_panel<T, layout, GEMM_MR, StorageLayout::ColumnMajor>(
+              A.submat(i_c + i, p_c, m_r, k_c), &packA[i * k_c]);
+        }
+
+        macro_kernel<T, layout, GEMM_MR, GEMM_NR, MICRO_KERNEL>(
+            m_c, k_c, n_c, packA, packB, C.submat(i_c, j_c, m_c, n_c));
+      } // End 3.rd loop around micro-kernel
+    } // End 4.th loop around micro-kernel
+  } // End 5.th loop around micro-kernel
+
+  std::free(packA);
+  std::free(packB);
+
+  return C;
+}
+
 } // namespace GEMM
